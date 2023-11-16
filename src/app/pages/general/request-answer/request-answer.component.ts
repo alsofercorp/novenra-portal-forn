bb0edc3f60e777a29ed476d270e84191507b44f0
Leonardo Silva <lcsilva80@yahoo.com.br>
2023-11-15 21:25:54 -0300

    FEAT: Criada a tela de resposta de solicitação

--- src/app/pages/general/request-answer/request-answer.component.ts
+++ src/app/pages/general/request-answer/request-answer.component.ts
@@ -1,3 +1,5 @@
+import { answerDataList } from './../../../../assets/data/answerData';
+import { IAnswerRequest } from './../../../interface/IAnswerRequest';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-answer.component.scss']
 })
 export class RequestAnswerComponent implements OnInit {
+  answerData: IAnswerRequest[] = answerDataList;
+
+  value: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  discountCalc(input: IAnswerRequest): number {
+    const totalValue: number = input.requestedQtty * input.unityValue;
+    input.ipiValue = totalValue * (input.ipiPercent);
+    const icms: number = totalValue * input.icmsPercent;
+    const discount: number = (totalValue + input.ipiValue + icms) * input.discount;
+
+    return (totalValue + input.ipiValue + icms) - discount;
   }
 
 }
